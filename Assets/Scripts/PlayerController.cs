d5c368a1a5e6ca6889dbd0eb477de33f37339069
TheFlamingoGames <theflamingogames@gmail.com>
2021-03-24 01:21:21 +0100

    New Character
    Added new animations, camera movement, and adjusted the physics and movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Physics
-    [SerializeField] float normalGravity;
-    [SerializeField] float fallGravity;
-    [SerializeField] float floatGravity;
+    [SerializeField] float normalGravity =3;
+    [SerializeField] float fallGravity =4;
+    [SerializeField] float floatGravity =0.5f;
     Rigidbody2D rigid;
+    Animator animator;
 
     //Jump
     [SerializeField] float groundTime = 0.2f;
@@ -26,9 +27,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speedX = 10f;
     float movementSpeed;
 
+    [SerializeField] float gcPosY = -0.01f;
+    [SerializeField] float gcScale = -0.02f;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -44,17 +48,16 @@ public class PlayerController : MonoBehaviour
     private void MovementInput() 
     {
         //Check if player hit the ground
-        Vector2 groundCheckPos = (Vector2)transform.position + new Vector2(0, -0.01f);
-        Vector2 groundCheckScale = (Vector2)transform.localScale + new Vector2(-0.02f, 0);
+        Vector2 groundCheckPos = (Vector2)transform.position + new Vector2(0, gcPosY);
+        Vector2 groundCheckScale = (Vector2)transform.localScale + new Vector2(gcScale, 0);
         bool booGround = Physics2D.OverlapBox(groundCheckPos, groundCheckScale, 0, collisionLayer);
-
         //Tick down the timers
         groundTimer -= Time.deltaTime;
         jumpTimer -= Time.deltaTime;
 
         //jump timer and coyote time activated for smoother gameplay
         if (Input.GetButtonDown("Jump")) { jumpTimer = jumpTime; }
-        if (booGround) { groundTimer = groundTime; }
+        if (booGround) {groundTimer = groundTime; }
 
         //If jump was pressed, and player touched the ground, jump
         if ((jumpTimer > 0) && (groundTimer > 0))
@@ -75,7 +78,39 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetButton("Jump"))
         {
-            Debug.Log("It keeps happening while I press the button");
+            
+        }
+
+        if (booGround)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                animator.SetTrigger("Walk");
+            }
+            else
+            {
+                animator.SetTrigger("Stop");
+            }
+            if (Input.GetButtonDown("Fire1"))
+            {
+                animator.SetTrigger("Duck");
+            }
+            if (Input.GetButtonUp("Fire1"))
+            {
+                animator.SetTrigger("DuckUp");
+            }
+        }
+        else 
+        {
+
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0) 
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
         }
     }
 
@@ -87,7 +122,5 @@ public class PlayerController : MonoBehaviour
             rigid.velocity = new Vector2(rigid.velocity.x, speedY);
         }
         rigid.velocity = new Vector2(movementSpeed, rigid.velocity.y);
-
-
     }
 }
